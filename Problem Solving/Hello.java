cbdf8fc39e83b7c9ffd94c6b84ac104286cc614a
kiran ajagekar <sanikaajagekar10@gmail.com>
2024-04-29 15:42:59 +0530

    update

--- Problem Solving/Hello.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-class Hello {
-	
-	public static void main(String args[]) {
-		System.out.println("Hiii");
-	}
-}
---
+++ problem_solving/Hello.java
@@ -0,0 +1,8 @@
+package problem_solving;
+import java.util.Scanner;
+class Hello {
+
+    public static void main(String args[]) {
+        System.out.println("Hiii");
+    }
+}
